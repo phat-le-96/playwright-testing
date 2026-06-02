47c7b2a6762db7394b6dabd0c126df04b921893e
Phat <Phatl2511@gmail.com>
2026-06-02 18:10:02 +0700

    feat: final stage

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,14 @@
+function cacCapChiaHetCho17(){
+    let pairs = [];
+
+    for (let i = 1; i <= 100; i++) {
+        for (let j = i + 1; j <= 100; j++) {
+            if ((i + j) % 17 === 0) {
+                pairs.push([i, j]);
+            }
+        }
+    }
+
+    console.log(`Total pairs: ${pairs.length}`);
+    return pairs;
+}
